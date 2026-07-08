7c359fca3ceab59a41ce5453dd802748dc588f8f
navinsaud <navinsaud0110@gmail.com>
2026-07-08 23:47:09 +0545

    add projects carts

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -23,4 +23,40 @@ export const Project=[
     img:"/images/cloudweb.jpeg",
     desc:"Top software development company and IT training institute. Get expert-led courses, custom software, and enterprise solutions.",
   },
+  {
+    id:5,
+    title:"E-commerce Website",
+    img:"/images/ecommerce.jpeg",
+    desc:"This is a full stack e-commerce website built using ReactJs.",
+  },
+   {
+    id:6,
+    title:"Online Food Ordering System",
+    img:"/images/food.jpeg",
+    desc:"Connects customers directly to restaurants via websites or mobile apps.",
+  },
+   {
+    id:7,
+    title:"Home Application",
+    img:"/images/home.jpeg",
+    desc:"To show you the status of your home and keep you up to date with what you may have missed.",
+  },
+   {
+    id:8,
+    title:"Cloud Web Nepal",
+    img:"/images/cloudweb.jpeg",
+    desc:"Top software development company and IT training institute. Get expert-led courses, custom software, and enterprise solutions.",
+  },
+  {
+    id:9,
+    title:"E-commerce Website",
+    img:"/images/ecommerce.jpeg",
+    desc:"This is a full stack e-commerce website built using ReactJs.",
+  },
+   {
+    id:10,
+    title:"Online Food Ordering System",
+    img:"/images/food.jpeg",
+    desc:"Connects customers directly to restaurants via websites or mobile apps.",
+  },
 ]
